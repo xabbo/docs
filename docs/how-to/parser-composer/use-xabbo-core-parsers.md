6d706f8d58a59a24335a5e6ebfdc879fa46974b0
rob <rob@b7c.io>
2024-10-11 02:57:02 +1300

    add using Xabbo.Core parsers

---
+++ docs/how-to/parser-composer/use-xabbo-core-parsers.md
@@ -0,0 +1,26 @@
+# How to: Use Xabbo.Core parsers
+
+The @Xabbo.Core library provides many parsers and composers that you can use in your extensions.
+Most of them implement @Xabbo.Messages.IParserComposer``1 and can therefore both be read from and
+written to packets. They are designed to work across different clients where possible.
+
+## Examples
+
+### Using core parsers
+
+Using the @Xabbo.Core.Avatar parser to read the list of avatars added to the room:
+
+[!code-csharp[](~/src/examples/core-parsers/minimal/Program.cs?name=parsers)]
+
+### Using core composers
+
+Using the @Xabbo.Core.Avatar composer to inject a client-side avatar into the room:
+
+[!code-csharp[](~/src/examples/core-parsers/minimal/Program.cs?name=composers)]
+
+### Using core parser/composers
+
+Using the @Xabbo.Core.Avatar parser/composer to modify all avatars in the room to have the same
+figure:
+
+[!code-csharp[](~/src/examples/core-parsers/minimal/Program.cs?name=parser-composers)]
---
+++ src/examples/core-parsers/minimal/Program.cs
@@ -0,0 +1,67 @@
+﻿using Xabbo;
+using Xabbo.GEarth;
+using Xabbo.Messages.Flash;
+using Xabbo.Core;
+
+var ext = new GEarthExtension(new GEarthOptions {
+    Name = "How to",
+    Description = "use core parsers",
+    Author = "xabbo"
+});
+
+// <parsers>
+ext.Intercept(In.Users, e => {
+    // Parsing an array of Avatars.
+    var avatars = e.Packet.Read<Avatar[]>();
+    foreach (var avatar in avatars)
+    {
+        // You can now access avatar.Id, avatar.Name, avatar.Location, etc.
+        Console.WriteLine(avatar.Name);
+    }
+});
+// </parsers>
+
+ext.Activated += () =>
+{
+    // <composers>
+    // Injecting a client-side user into the room.
+    Id id = 1000; int index = 1000;
+    ext.Send(In.Users, new Avatar[] {
+        new User(id, index)
+        {
+            Name = "xabbo",
+            Motto = "hello from xabbo/core",
+            Location = (6, 6, 0),
+            Figure = "hr-3090-42.hd-180-1.ch-3110-64-1408.lg-275-64.ha-1003-64",
+            Gender = Gender.Male
+        }
+    });
+    // </composers>
+};
+
+// <parser-composers>
+ext.Intercept(In.Users, e =>
+{
+    // Read the number of avatars in the packet.
+    int n = e.Packet.Read<Length>();
+    // Loop through each avatar.
+    for (int i = 0; i < n; i++)
+    {
+        // Modify each avatar.
+        e.Packet.Modify<Avatar>(avatar => {
+            // If the avatar is a user...
+            if (avatar is User user)
+            {
+                // Replace the avatar's name, figure and gender.
+                user.Name = "xabbo";
+                user.Figure = "hr-3090-42.hd-180-1.ch-3110-64-1408.lg-275-64.ha-1003-64";
+                user.Gender = Gender.Male;
+            }
+            // Return the modified avatar.
+            return avatar;
+        });
+    }
+});
+// </parser-composers>
+
+ext.Run();
